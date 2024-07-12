183a6d383e6a8fde94bc1457cccbf3ca8e922f77
mikaelalundstrom <mickismickis@hotmail.se>
2024-07-12 10:43:55 +0200

    changed some let variables to const

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // Exercise 1
 console.log("Exercise 1");
 
-let recipeName = document.querySelector("#recipe-name");
+const recipeName = document.querySelector("#recipe-name");
 console.log(`Recipe name: ${recipeName.innerText}`);
 
 // Exercise 2
@@ -13,7 +13,7 @@ console.log(`The tag used to display the Recipe name is: ${recipeName.tagName}`)
 // Exercise 3
 console.log("Exercise 3");
 
-let descriptionRef = document.querySelector(".description");
+const descriptionRef = document.querySelector(".description");
 //if no set value, log computed style
 if (!descriptionRef.style.fontSize) {
   console.log(`The font size of the description is: ${getComputedStyle(descriptionRef).fontSize}`);
@@ -25,7 +25,7 @@ if (!descriptionRef.style.fontSize) {
 // Exercise 4
 console.log("Exercise 4");
 
-let imgRef = document.querySelector("img");
+const imgRef = document.querySelector("img");
 console.log(`The alt attribute for the img says: ${imgRef.alt}`);
 
 // Exercise 5
@@ -41,12 +41,12 @@ console.log(imgRefObject);
 // Exercise 6
 console.log("Exercise 6");
 
-let ingredientsListPasteRef = document.querySelector(".ingredients-list-paste");
-
+const ingredientsListPasteRef = document.querySelector(".ingredients-list-paste");
 console.log(`The paste has ${ingredientsListPasteRef.children.length} ingredients.`);
 
 // Exercise 7
 console.log("Exercise 7");
+
 let pasteIngredientEl4 = ingredientsListPasteRef.children[3].outerHTML;
 console.log(`The fourth element in the paste ingredients list is "${pasteIngredientEl4}"`);
 
@@ -54,7 +54,7 @@ console.log(`The fourth element in the paste ingredients list is "${pasteIngredi
 console.log("Exercise 8");
 
 let instructions = [];
-let instructionsRefs = document.querySelectorAll(".instructions-list>li");
+const instructionsRefs = document.querySelectorAll(".instructions-list>li");
 
 instructionsRefs.forEach((instruction, index) => {
   instructions.push({
